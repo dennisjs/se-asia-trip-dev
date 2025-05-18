3bc23e1a3232a413b2d04792a8b926e9d0e655f0
dennisjs <dennisj@outlook.com>
2025-05-17 18:20:01 -0700

    Update timeline.js

--- js/timeline.js
+++ js/timeline.js
@@ -21,7 +21,7 @@ async function loadTimeline() {
         <div class="photos">
           ${photos.map(photo => `
             <figure>
-              <img src="images/${photo.id}.jpg" alt="${photo.caption}" onclick="showOverlay('images/${photo.id}.jpg', '${photo.caption.replace(/'/g, "\\'")}')">
+              <img src="images/${photo.id}.jpg" alt="${photo.caption}" data-photo-index="${i}" class="photo-thumb">
               <figcaption>${photo.caption}</figcaption>
             </figure>
           `).join("")}
