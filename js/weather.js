6a163c1823d9cfb13f04c69070569a2d2c61f4e3
dennisjs <dennisj@outlook.com>
2025-05-21 09:55:27 -0700

    debug1

--- js/weather.js
+++ js/weather.js
@@ -9,7 +9,7 @@ function updateWeatherBox(lat, lng, place) {
     .then(weather => {
       console.log("weather string:", weather);
       console.log("temp: ", weather.current.temp);
-      const weatherStr = `${Math.round(weather.current.temp)}°F, ${weather.weather[0].description}`;
+      const weatherStr = `${Math.round(weather.current.temp)}°F, ${weather.current.weather[0].description}`;
       box.innerHTML = `<strong>My Current Location:</strong><br>${place}<br>⛅ ${weatherStr}`;
     })
     .catch(err => {
