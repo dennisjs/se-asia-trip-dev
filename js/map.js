9a9586200f3b76fa009cee1c5ee12c74a5616983
dennisjs <dennisj@outlook.com>
2025-05-21 09:00:18 -0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -63,7 +63,9 @@ window.initMapWithPhotos = function () {
 
     // Floating weather box
     const infoBox = document.createElement('div');
+    infoBox.id = 'location-box';
     infoBox.className = 'location-info-box';
+
     infoBox.innerHTML = "<strong>My Current Location:</strong><br>" + place + "<br>Loading weather...";
     document.body.appendChild(infoBox);
 
