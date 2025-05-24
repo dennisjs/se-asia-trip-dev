00422336fc75b6e4c4a30cd2697ca3b80f15a842
dennisjs <dennisj@outlook.com>
2025-05-24 10:52:17 -0700

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -163,7 +163,24 @@ window.initMapWithPhotos = function () {
         if (!infoBox.contains(e.target) && e.target !== infoBtn) {
           infoBox.classList.remove("active");
         }
-      });
+      
+        // Map Info toggle
+        const infoBtn = document.getElementById("map-info-btn");
+        const infoBox = document.getElementById("map-info-box");
+
+        if (infoBtn && infoBox) {
+          infoBtn.addEventListener("click", () => {
+            infoBox.classList.toggle("active");
+          });
+
+          document.addEventListener("click", (e) => {
+            if (!infoBox.contains(e.target) && e.target !== infoBtn) {
+              infoBox.classList.remove("active");
+            }
+          });
+        }
+
+});
     }
 
 };
