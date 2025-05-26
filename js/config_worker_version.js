31980813f5ce24526592852489d003efb70baacd
dennisjs <dennisj@outlook.com>
2025-05-25 20:44:14 -0700

    Delete js/config_worker_version.js

--- js/config_worker_version.js
+++
@@ -1,6 +0,0 @@
-
-window.CONFIG = {
-  MAPBOX_TOKEN: "pk.eyJ1IjoiZGVubmlzanMiLCJhIjoiY21iM3RsbDBkMHlkeTJpcTNqdXgzbmJ6dSJ9.Wm3ylbaIO2j5TUorrNTdmg",
-  OPENWEATHER_KEY: "https://silent-snow-b58a.dennisj.workers.dev"
-};
-
